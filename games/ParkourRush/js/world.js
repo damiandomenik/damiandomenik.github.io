e8eee9145d8053368a642affb020e9bca7919480
frck <frick.damian92@gmail.com>
2026-08-01 00:43:13 +0200

    Update world.js

--- games/ParkourRush/js/world.js
+++ games/ParkourRush/js/world.js
@@ -1,90 +1,108 @@
 import Obstacle from "./obstacle.js";
 
 
-export default class World{
+export default class World {
 
 
-constructor(scene){
+    constructor(scene){
 
-    this.scene = scene;
+        this.scene = scene;
 
-    this.obstacles = [];
+        this.obstacles = [];
 
-    this.group = scene.physics.add.group();
-
-
-    scene.time.addEvent({
-
-        delay:1500,
-
-        loop:true,
-
-        callback:()=>{
-
-            this.spawn();
-
-        }
-
-    });
+        this.group = scene.physics.add.group();
 
 
-}
+        scene.time.addEvent({
+
+            delay:3000,
+
+            loop:true,
+
+            callback:()=>{
+
+                if(scene.running){
+
+                    this.spawn();
+
+                }
+
+            }
+
+        });
 
 
-
-spawn(){
-
-
-    let types=[
-        "block",
-        "laser"
-    ];
-
-
-    let type =
-    types[
-        Phaser.Math.Between(
-            0,
-            types.length-1
-        )
-    ];
-
-
-    let obstacle = new Obstacle(
-        this.scene,
-        1250,
-        490,
-        type
-    );
-
-
-    this.obstacles.push(
-        obstacle
-    );
-
-
-    this.group.add(
-        obstacle.sprite
-    );
-
-
-}
+    }
 
 
 
-update(speed){
+    spawn(){
 
 
-    this.obstacles.forEach(
-        o=>{
-
-            o.sprite.x -= speed * 0.3;
-
-        }
-    );
+        let types = [
+            "block",
+            "laser"
+        ];
 
 
-}
+        let type = types[
+            Phaser.Math.Between(
+                0,
+                types.length - 1
+            )
+        ];
+
+
+
+        let obstacle = new Obstacle(
+            this.scene,
+            1400,
+            490,
+            type
+        );
+
+
+
+        this.obstacles.push(
+            obstacle
+        );
+
+
+
+        this.group.add(
+            obstacle.sprite
+        );
+
+
+    }
+
+
+
+    update(speed){
+
+
+        this.obstacles.forEach(
+
+            o=>{
+
+
+                o.sprite.x -= speed * 0.015;
+
+
+
+                if(o.sprite.x < -200){
+
+                    o.sprite.destroy();
+
+                }
+
+
+            }
+
+        );
+
+
+    }
 
 
 }
