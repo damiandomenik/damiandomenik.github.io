b4673247be819eecf631a617d1916c219b4b4442
frck <frick.damian92@gmail.com>
2026-07-31 23:52:34 +0200

    Update ui.js

---
+++ games/ParkourRush/js/ui.js
@@ -0,0 +1,50 @@
+export default class UI {
+
+
+    constructor(scene){
+
+        this.scene = scene;
+
+
+        this.text = scene.add.text(
+            20,
+            20,
+            "",
+            {
+                fontSize: "32px",
+                fill: "#ffffff"
+            }
+        );
+
+    }
+
+
+
+    showStart(){
+
+        this.text.setText(
+            "READY...\n\n3\n2\n1\nGO!"
+        );
+
+    }
+
+
+
+    hide(){
+
+        this.text.setText("");
+
+    }
+
+
+
+    updateDistance(distance){
+
+        this.text.setText(
+            "Distance: " + distance + " m"
+        );
+
+    }
+
+
+}
