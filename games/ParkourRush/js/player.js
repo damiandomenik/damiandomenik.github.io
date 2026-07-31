75497628d95377a0183cfe5f99b49d5fb977db29
frck <frick.damian92@gmail.com>
2026-07-31 23:48:43 +0200

    Update player.js

--- games/ParkourRush/js/player.js
+++ games/ParkourRush/js/player.js
@@ -3,31 +3,25 @@ export default class Player{
 
 constructor(scene,x,y){
 
-this.scene=scene;
+
+this.scene = scene;
 
 
-this.sprite=
-scene.add.rectangle(
-x,
-y,
-40,
-60,
-0x00ffff
+this.sprite = scene.add.rectangle(
+    x,
+    y,
+    45,
+    60,
+    0x00ffff
 );
 
 
 scene.physics.add.existing(
-this.sprite
+    this.sprite
 );
 
 
-this.body=this.sprite.body;
-
-
-this.body.setCollideWorldBounds(true);
-
-
-this.jumping=false;
+this.sprite.body.setCollideWorldBounds(true);
 
 
 }
@@ -39,28 +33,27 @@ update(keys){
 
 if(
 keys.jump.isDown &&
-this.body.blocked.down
+this.sprite.body.blocked.down
 ){
 
-this.body.setVelocityY(-550);
+this.sprite.body.setVelocityY(-600);
 
 }
 
 
 if(keys.duck.isDown){
 
-this.sprite.height=30;
+this.sprite.height = 30;
 
 }
 else{
 
-this.sprite.height=60;
+this.sprite.height = 60;
 
 }
 
 
 }
-
 
 
 }
