0d27f36e5d8fc6c72f0a9665435d84640bca6310
frck <frick.damian92@gmail.com>
2026-08-01 00:24:31 +0200

    Update obstacle.js

--- games/ParkourRush/js/obstacle.js
+++ games/ParkourRush/js/obstacle.js
@@ -4,48 +4,36 @@ export default class Obstacle{
 constructor(scene,x,y,type){
 
 
-let color=0xff00aa;
+let color = 0xff00aa;
 
 
-if(type==="block")
-color=0xff8800;
+if(type==="block"){
+    color = 0xff8800;
+}
 
 
-if(type==="laser")
-color=0xff0000;
+if(type==="laser"){
+    color = 0xff0000;
+}
 
 
-this.sprite=
-scene.add.rectangle(
-x,
-y,
-60,
-90,
-color
+this.sprite = scene.add.rectangle(
+    x,
+    y,
+    60,
+    90,
+    color
 );
 
 
 scene.physics.add.existing(
-this.sprite
+    this.sprite
 );
 
 
-this.sprite.body.setVelocityX(-300);
+this.sprite.body.setImmovable(true);
 
 
 }
-
-
-
-update(){
-
-if(this.sprite.x < -100){
-
-this.sprite.destroy();
-
-}
-
-}
-
 
 }
